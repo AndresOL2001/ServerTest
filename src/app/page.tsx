4a2f4bf93fa309cc6b3b908a9e007ee09578ca68
Andres_Olaje_ <andres.olaje@tisconsulting.org>
2023-03-23 10:54:31 -0700

    linter config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image'
-import { Imprima, Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { Inter } from 'next/font/google'
+import styles from './styles/page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
 
   return (
