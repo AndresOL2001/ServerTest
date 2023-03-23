93e0c33f2a2ca2b64c69a7260655131e98b45b3f
eduardoMtzCh <97627897+eduardoMtzCh@users.noreply.github.com>
2023-03-22 19:32:44 -0700

    Setting global variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors:{
+      'primary':'#0072B5',
+      'secondary':'#1B3156',
+      'light':'#EFEFEF',
+      'accent':'#EDD30F',
+      'dark-border':'#183860',
+      'light-border':'#D4E1E8',
+      'dark':'#4E4E4E',
+      'muted':'#CECECE',
+      'success':'#2B690B',
+      'info':'#305696',
+      'danger':'#9F2E13',
+      'warning':'#7B5C09'
+    },
     extend: {},
   },
   plugins: [],
