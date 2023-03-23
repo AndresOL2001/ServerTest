5c276322c24fda24932f175a8c9cfdf273e6e5fd
sofia-gastelum <sgastelum@aviadalabs.com>
2023-03-23 13:02:02 -0700

    typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,28 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Roboto', sans-serif, 'arial', 'Helvetica Neue', 'Noto Sans'],
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: ['1.5rem', {
+        fontWeight: '400',
+      }],
+      '2xl': ['2rem', {
+        fontWeight: '400',
+      }],
+      '3xl': ['2.5rem', {
+        fontWeight: '400',
+      }],
+      '4xl': ['3rem', {
+        fontWeight: '400',
+      }],
+      '5xl': ['3.375rem', {
+        fontWeight: '400',
+      }],
+    },
     extend: {},
   },
   plugins: [],
