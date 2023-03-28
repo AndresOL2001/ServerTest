2ca89ebcb4fe20ea7b650914265bf591be381adc
Janeth <janethtrs98@gmail.com>
2023-03-28 09:43:16 -0700

    CU-865bzzx8r_Cards customize panel

--- src/app/components/Atoms/Panel/Panel.mocks.ts
+++ src/app/components/Atoms/Panel/Panel.mocks.ts
@@ -1,8 +1,15 @@
+import { blue, lightBlue } from '@material-ui/core/colors';
+import { light } from '@material-ui/core/styles/createPalette';
 import { IPanel } from './Panel';
 
 const base: IPanel = {
   width: 600,
   height: 783,
+  background_color: 'lightBlue',
+  border_color: 'blue',
+  border_radius:10,
+  border_weight: 1,
+  drop_shadow: ['10','0','10','red'],
 };
 
 export const mockPanelProps = {
--- src/app/components/Atoms/Panel/Panel.tsx
+++ src/app/components/Atoms/Panel/Panel.tsx
@@ -3,16 +3,28 @@ import styles from './Panel.module.css';
 export interface IPanel {
   width: number;
   height: number;
+  background_color: string;
+  border_color: string;
+  border_weight: number;
+  border_radius: number;
+  drop_shadow: string[];
+
 }
 
-const Panel: React.FC<IPanel> = ({ width,height}) => {
+const Panel: React.FC<IPanel> = ({ width,height,background_color,border_color,border_radius,border_weight,drop_shadow}) => {
   return (
     <div
       style={{
         height: `${height}px`,
         width: `${width}px`,
+        backgroundColor:`${background_color}`,  
+        borderColor: `${border_color}`,
+        borderRadius: `${border_radius}px`,
+        boxShadow: `${drop_shadow[0]}px ${drop_shadow[1]}px ${drop_shadow[2]}px ${drop_shadow[3]}`,
+        borderWidth: `${border_weight}px`,
+        
       }}
-      className={`border-blue-300 border-2 border-solid rounded-[10px] bg-white`}>
+      className={`border-solid`}>
     </div>
   ); 
 };
