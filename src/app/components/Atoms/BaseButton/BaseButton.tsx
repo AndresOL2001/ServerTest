79f2f462710b056bf52be86d99f46bb20a2edbe6
Esmeralda <esmencinas1@gmail.com>
2023-04-02 15:05:10 -0700

    CU-865bx5jd3_Buttons - Buttons - Added Buttons

---
+++ src/app/components/Atoms/BaseButton/BaseButton.tsx
@@ -0,0 +1,55 @@
+import './BaseButton.module.css';
+import React, {useState} from 'react';
+
+export interface IBaseButton {
+  sampleTextProp: string;
+  onclickProp: () => void;
+  disable: boolean;
+  size?: 'normal' | 'small' | 'mini';
+  isDragging?: 'dragging' | '';
+
+}
+
+const BaseButton: React.FC<IBaseButton>  = ({ 
+  onclickProp, 
+  sampleTextProp, 
+  disable,
+  size = 'mini'}) => {
+    const [isDragging, setIsDragging] = useState(false);
+
+    const handleDragStart = (event: React.DragEvent<HTMLButtonElement>) => {
+      event.dataTransfer.setData('text/plain', '');
+      setIsDragging(true);
+    };
+  
+    const handleDragEnd = () => {
+      setIsDragging(false);
+    };
+  
+    return (
+      <button
+     draggable
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      disabled={disable ? true : false}
+      className={`primary button--${size} primary--${isDragging}` } onClick={onclickProp}>
+      {sampleTextProp}
+    </button>
+  );
+};
+
+/*const BaseButton: React.FC<IBaseButton> = (
+  { onclickProp, 
+    sampleTextProp, 
+    disable,
+    size = 'mini'}) => {
+  return (
+  <button 
+  disabled={disable ? true : false}
+  className={`primary button--${size}`} onClick={onclickProp}>
+    {sampleTextProp}
+  </button>)
+};*/
+
+
+export default BaseButton;
