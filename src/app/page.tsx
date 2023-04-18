06e59f4e18d48be458709b5591a223ffdc974aa4
AndresOL2001 <70553068+AndresOL2001@users.noreply.github.com>
2023-04-17 20:38:25 -0700

    TestServe Error 2 - Update Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
 
   return (
-    <main className={styles.main}>
-      <Navbar
-        backgroundColor="#1B3156"
-        height={50}
-        items={[
-          'Home',
-          'About us',
-          'Our services',
-          'Blog'
-        ]}
-        logo="/logo-white.svg"
-        userInfo={{
-          id: '0',
-          occupation: 'Software Engineer',
-          username: 'Johnathan'
-        }}
-        userNav
-        width={150}
-      />
-    </main>
+   <main className={styles.main}></main>
   )
 }
