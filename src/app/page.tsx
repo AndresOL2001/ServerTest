37617250871f32c2234aa002484b3b0fcc100f1e
AndresOL2001 <70553068+AndresOL2001@users.noreply.github.com>
2023-04-17 21:27:50 -0700

    USE CLIENTTTTT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 //@ts-nocheck
+"use client"
 import Image from 'next/image'
 import { Imprima, Inter } from 'next/font/google'
 import styles from './page.module.css'
@@ -10,6 +11,23 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
    return <>
      <Sidebar/>
+      <Navbar
+  backgroundColor="#1B3156"
+  isUserNav
+  logoUrl="/logo-white.svg"
+  navItems={[
+    'Home',
+    'About us',
+    'Our services',
+    'Blog'
+  ]}
+  primary
+  showLogo
+  userInformation={{
+    occupation: 'Software Engineer',
+    username: 'Johnathan'
+  }}
+/>
       <main className={styles.main}>
       <h1>Servidor de prueba activao</h1>
    </main>;
