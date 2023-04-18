2b2ebca8d772f25b4858fca5c65421cfc4906866
AndresOL2001 <70553068+AndresOL2001@users.noreply.github.com>
2023-04-17 21:20:07 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,23 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
    return <>
+      <Navbar
+  backgroundColor="#1B3156"
+  isUserNav
+  logoUrl="/logo-white.svg"
+  navItems={[
+    'Home',
+    'About us',
+    'Our services',
+    'Blog'
+  ]}
+  primary
+  showLogo
+  userInformation={{
+    occupation: 'Software Engineer',
+    username: 'Johnathan'
+  }}
+/>
       <main className={styles.main}>
       <h1>Servidor de prueba activao</h1>
    </main>;
