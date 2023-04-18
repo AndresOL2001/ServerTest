9829ef917c28d792525b6773d9d2b0fb620f670f
AndresOL2001 <70553068+AndresOL2001@users.noreply.github.com>
2023-04-17 21:26:12 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,13 @@ import Image from 'next/image'
 import { Imprima, Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Navbar from './components/Molecules/Navbar/Navbar'
+import Sidebar from './components/Molecules/Sidebar/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
    return <>
-      <Navbar
-  backgroundColor="#1B3156"
-  isUserNav
-  logoUrl="/logo-white.svg"
-  navItems={[
-    'Home',
-    'About us',
-    'Our services',
-    'Blog'
-  ]}
-  primary
-  showLogo
-  userInformation={{
-    occupation: 'Software Engineer',
-    username: 'Johnathan'
-  }}
-/>
+     <Sidebar/>
       <main className={styles.main}>
       <h1>Servidor de prueba activao</h1>
    </main>;
