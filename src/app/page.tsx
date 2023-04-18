ebaab6e0d083d55d5ef82ed1052c196a76541053
AndresOL2001 <70553068+AndresOL2001@users.noreply.github.com>
2023-04-18 10:30:38 -0700

    Sidebar deploy 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
 
   return (
-    <main className={styles.main}>
-      <Navbar
-        backgroundColor="#1B3156"
-        height={50}
-        items={[
-          'Home',
-          'About us',
-          'Our services',
-          'Blog'
-        ]}
-        logo="/logo-white.svg"
-        userInfo={{
-          id: '0',
-          occupation: 'Software Engineer',
-          username: 'Johnathan'
-        }}
-        userNav
-        width={150}
-      />
-    </main>
+    <>
+      <Sidebar/>
+      <main className={styles.main}>
+      </main>
+     </>
   )
 }
